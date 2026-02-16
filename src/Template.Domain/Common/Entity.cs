e8e8e3bd81b4cb447824ec9cd87f0779f5413e92
Julius Bartolome <juliusbartolome@duck.com>
2026-02-16 11:46:21 +0800

    feat: Add NotMapped attribute to DomainEvents property in Entity class

--- src/Template.Domain/Common/Entity.cs
+++ src/Template.Domain/Common/Entity.cs
@@ -1,9 +1,12 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace Template.Domain.Common;
 
 public abstract class Entity
 {
     private readonly List<DomainEvent> _domainEvents = [];
 
+    [NotMapped]
     public IReadOnlyCollection<DomainEvent> DomainEvents => _domainEvents.AsReadOnly();
 
     protected void RaiseDomainEvent(DomainEvent domainEvent) => _domainEvents.Add(domainEvent);
